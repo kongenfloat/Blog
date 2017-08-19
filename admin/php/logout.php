02608d043d35cd9c2d8a5792427a4fc66dca58e8
kongenfloat <andreflaate2@gmail.com>
2017-08-19 23:44:57 +0200

    Fixed logout message bug
    Logout message was not being displayed. Solved by starting a new session
    i logout.php after previous session was destroyed. The new session does
    not yet contain any information if a user is logged in or not.

--- admin/php/logout.php
+++ admin/php/logout.php
@@ -2,6 +2,9 @@
    session_start();
    unset($_SESSION["user"]);
    session_destroy();
+
+   //Start new session for logout message
+   session_start();
    $_SESSION['success'] = "<div class='success'><p>Du er nå logget ut</p></div>";
    header('Location: http://splend-it.no/admin/');
    exit();
