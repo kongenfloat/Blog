248797d9767853fd59591be0fe77840c21a03f3a
kongenfloat <andreflaate2@gmail.com>
2017-08-19 17:58:09 +0200

    Implemented all.php
    Implemented all.php as a page to show all blog posts for admin. Admin
    will have option to edit and delete blog posts from here.

---
+++ admin/pages/all.php
@@ -0,0 +1,38 @@
+<?php
+
+	$list = get_all_posts();
+
+	foreach ($list as $post => $value) {
+		$id = $list[$post]['id'];
+ 		$heading = $list[$post]['heading'];
+        $blog_text = $list[$post]['blog_text'];
+        $image = $list[$post]['image'];
+        $created =  $list[$post]['created'];
+
+
+        //Process $blog_text for minimized post if string length is longer than 900 characters
+        if(strlen ($blog_text) > 900){
+        	$blog_text = substr ($blog_text , 0, 900);
+        	$blog_text = $blog_text . "...";	
+        }
+        
+        //$updated = $post['updated'];
+        //$likes = $post['likes'];
+        //$views = $post['views'];
+        echo "<div class='minimized-post margin-top-4'>";
+    		echo "<div class='minimized-post-left'>";
+    			echo "<h2>$heading</h2>";
+    			echo "<p>$created</p>";
+    		echo "</div>";
+    		echo "<div class='minimized-post-right'>";
+		        //echo "<p>$likes</p>";
+		        //echo "<p>$views</p>";
+		        echo "<p id='minimized-text'>$blog_text</p>";
+		        echo "<a href='?page=edit&id=$id'> Endre </a>";
+                echo "<a href='?page=delete&id=$id'> Slett </a>";
+    		echo "</div>";
+        echo "</div>";
+	}
+
+
+?>
