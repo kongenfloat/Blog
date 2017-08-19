2827e9ac803de5e1248d886c349731005dfea02e
kongenfloat <andreflaate2@gmail.com>
2017-08-19 15:40:22 +0200

    Implemented show.php
    Implemented show.php which is responsible for displaying only one blog
    post. This page is included in index.php if an id is provided.

--- js/ajax.js
+++ js/ajax.js
@@ -11,8 +11,12 @@ function add_like(button) {
 	xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
-		hide("likes_label");
-		document.getElementById("likes").innerHTML = this.responseText;	
+		if(this.responseText === "exists"){
+			document.getElementById("error").innerHTML = "Du har allerede liket dette blogginnlegget";
+		}else{
+			document.getElementById("likes").innerHTML = this.responseText;
+			hide("likes_label");
+		}
 	}
 	};
 	xhttp.open("GET", "php/add_like.php?id="+id, true);
---
+++ pages/show.php
@@ -0,0 +1,33 @@
+<?php
+
+		$id = $_GET['id'];
+
+		add_view($id);
+
+	    $post = get_post($id);
+        $heading = $post['heading'];
+        $blog_text = $post['blog_text'];
+        $image = $post['image'];
+        $created = $post['created'];
+        $updated = $post['updated'];
+        $likes = $post['likes'];
+        $views = $post['views'];
+
+        echo "<h2 class='center-text'>$heading</h2>";
+        echo "<p class='center-text'>$created</p>";
+        echo "<p class='center'>$blog_text</p>";
+        echo "<img src='$image' class='center margin-top-4 post-img'>";
+        echo "<p class='center-text'>Views: $views</p>";
+        echo "<label for='likes' id='likes_label'>Likes: </label>";
+        echo "<p id='likes' class='center-text'>$likes</p>";
+        
+        //Check to see if the user has already liked this blog post
+        if(check_ip($id) == true){
+        	echo "<p class='center-text' style='color: green;'>Du har allerede likt dette blogginnlegget</p>";	
+        }else{
+        	echo "<button id='like' class='center' value='$id' onclick='add_like(this)'>Like</button>";
+        }
+		        
+
+        //echo "<p>$updated</p>";
+?>
