316a025b7838175c32428a8f8e991f07e65055af
kongenfloat <andreflaate2@gmail.com>
2017-08-19 15:25:22 +0200

    Implemented admin.php and index.php
    Added admin.php and index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+include_once("access/database_functions.php");
+include_once("php/functions.php");
+?>
+
+<!doctype html>
+<html>
+<head>
+    <title>Blogg</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/ajax.js"></script>
+</head>
+
+<body>
+<div id="container">
+    <div id="header">
+        <h1 class="center-text">Trollweb blogg oppgave</h1>
+    </div>
+    <div id="content">
+
+
+        <?php 
+            //Check if user has chosen a specific post
+            //else show list of blog posts
+            if(isset($_GET['id'])){
+                include("pages/show.php");   
+            }else{
+                include("pages/list.php");
+            }
+        ?>
+
+    </div>
+</form>
+</div>
+</body>
+</html>
